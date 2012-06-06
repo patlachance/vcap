46e183d873e889c620252e715ca117b16ac47e88
Your Name <patlachance@gmail.com>
2012-06-06 20:42:51 +0000

    Fixed IP

--- bin/vcap_fix_cfg.sh
+++ bin/vcap_fix_cfg.sh
@@ -19,7 +19,7 @@ cd ~/cloudfoundry/.deployments/devbox/config
 
 # Editing deployment files
 FILES=`ls -1 *.yml`
-sed -i.bak "s/api.vcap.me/$URI/;s/10.48.41.240/$IP/;s/localhost:4222/$IP:4222/" $FILES
+sed -i.bak "s/api.vcap.me/$URI/;s/127.0.0.1/$IP/;s/localhost:4222/$IP:4222/" $FILES
 
 # faking /etc/hosts to be able to ping $URI locally in the ec2 environment
 sudo sh -c "echo \"$IP $URI\" >> /etc/hosts"
