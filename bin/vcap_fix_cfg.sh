5d42c03de2c19c9320d46a60298c0d5ce0252ed6
Your Name <patlachance@gmail.com>
2012-06-06 20:27:11 +0000

    Added config file fix script

---
+++ bin/vcap_fix_cfg.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+usage() {
+  cat <<EOT
+
+  Usage: `basename $0` <URL> <IP>
+
+EOT
+  exit 1
+}
+
+[ $# -ne 2 ] && usage
+
+URI=$1
+IP=$2
+
+CURDIR=`pwd`
+cd ~/cloudfoundry/.deployments/devbox/config
+
+# Editing deployment files
+FILES=`ls -1 *.yml`
+sed -i.bak "s/api.vcap.me/$URI/;s/10.48.41.240/$IP/;s/localhost:4222/$IP:4222/" $FILES
+
+# faking /etc/hosts to be able to ping $URI locally in the ec2 environment
+sudo sh -c "echo \"$IP $URI\" >> /etc/hosts"
+
+cd $CURDIR
+
